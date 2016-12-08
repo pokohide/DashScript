2f5a8afbfa441f312e4686dd334a6c45d7c54dfa
pokohide <hyde31415@gmail.com>
2016-12-08 17:14:40 +0900

    add config file

---
+++ config.js
@@ -0,0 +1,9 @@
+require('dotenv').config()
+
+module.exports = {
+  AMAZON: {
+    EMAIL: process.env.AMAZON_JP_EMAIL,
+    PASSWORD: process.env.AMAZON_JP_PASSWORD,
+    ITEM: process.env.AMAZON_JP_ITEM_URL
+  }
+}
