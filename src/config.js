8cd23300116843f649b45cac6f7cceb5965ab51c
pokohide <hyde31415@gmail.com>
2016-12-08 20:10:45 +0900

    フォルダ構成変えた

--- src/config.js
+++ src/config.js
@@ -5,5 +5,6 @@ module.exports = {
     EMAIL: process.env.AMAZON_JP_EMAIL,
     PASSWORD: process.env.AMAZON_JP_PASSWORD,
     ITEM: process.env.AMAZON_JP_ITEM_URL
-  }
+  },
+  PRODUCTION: process.env.PRODUCTION
 }
---
+++ src/dash-script.js
@@ -0,0 +1,114 @@
+const fs = require('fs'),
+      system = require('system'),
+      args = system.args
+
+class DashScirpt {
+  constructor(args) {
+    this.init()
+    this.email = args[1]
+    this.password = args[2]
+    this.item = args[3]
+    this.funcs = [
+      this.visit,
+      this.clickSignInButton//,
+      //this.signIn,
+      //this.openItemPage,
+      //this.addToCart
+    ]
+  }
+
+  init() {
+    this.page = require('webpage').create()
+    this.page.viewportSize = {width: 1920, height: 1080}
+    this.page.settings.userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'
+    this.page.settings.javascriptEnabled = true
+    this.page.settings.loadImages = false
+    phantom.cookiesEnabled = true
+    phantom.javascriptEnabled = true
+
+    this.page.onCallback = (data) => {
+      if(data === 'DOMContentLoaded') this.next()
+    }
+
+    this.page.onInitialized = () => {
+      this.page.evaluate(() => {
+        document.addEventListener('DOMContentLoaded', () => {
+          window.callPhantom('DOMContentLoaded')
+        }, false)
+      })
+    }
+  }
+
+  /* STEP1: サイトに訪れる */
+  visit() {
+    console.log('Open Top Page')
+    const that = this
+    this.page.open('https://amazon.co.jp', (status) => {
+      that.page.render('visit.png')
+    })
+  }
+
+  /* STEP:2 ログインボタンをクリック */
+  clickSignInButton() {
+    console.log('Click Signin Button')
+    const that = this
+    that.page.evaluate(() => {
+      console.log('hoge')
+      document.getElementById('nav-link-yourAccount').click()
+      that.page.render('click.png')
+      phantom.exit()
+    })
+  }
+
+  /* STEP3: ログインする */
+  signIn() {
+    console.log('sign in')
+    this.page.render('signin.png')
+    this.page.evaluate((email, password) => {
+      document.getElementById('ap_email').value = email
+      document.getElementById('ap_password').value = password
+      document.getElementById('signInSubmit').submit()
+    }, this.email, this.password)
+  }
+
+  /* STEP4: open Item Page */
+  openItemPage() {
+    console.log('open item page')
+    this.page.render('item.png')
+    this.page.open('https://www.amazon.co.jp/Amazon-W87CUN-Fire-TV-Stick/dp/B00ZVNYLS8/', (status) => {})
+  }
+
+  /* STEP5: add to cart */
+  addToCart() {
+    console.log('add to cart')
+    this.page.render('cart.png')
+    this.page.evaluate(() => {
+      document.getElementById('add-to-cart-button').click()
+    })
+  }
+
+  /* STEP6: 支払いページへ */
+  payment() {
+    console.log('Proceed to Payment')
+    this.page.render('payment.png')
+    this.page.open('https://www.amazon.co.jp/gp/buy/spc/handlers/display.html', (status) => {})
+  }
+
+  run() {
+    this.func = this.funcs.shift()
+    this.func()
+  }
+
+  next() {
+    this.func = this.funcs.shift()
+    if(this.func !== undefined) {
+      this.func()
+    } else {
+      this.page.onCallback = function(){}
+    }    
+  }
+}
+
+const d = new DashScirpt(args)
+d.run()
+
