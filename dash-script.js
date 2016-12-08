7c019713bb66aae55611155b8c2d08ed2ed902c9
pokohide <hyde31415@gmail.com>
2016-12-09 01:57:09 +0900

    商品ページまで行けた

--- dash-script.js
+++ dash-script.js
@@ -3,7 +3,7 @@ var system = require('system'),
     args = system.args,
     page = require('webpage').create()
 
-var email = args[1], password = args[2]
+var email = args[1], password = args[2], itemUrl = args[3]
 
 page.viewportSize = {
   width: 1920,
@@ -52,36 +52,39 @@ new funcs([
   function() {
     console.log('Open Top Page')
     page.open('https://amazon.co.jp', function(status) {
-      page.render('visit.png')
+      page.render('images/visit.png')
     })
   },
   function() {
     console.log('Click Signin Button')
-    page.evaluate(function() {
-      document.getElementById('nav-link-yourAccount').click()
-    })
-    page.render('click.png')
+    setTimeout(function() {
+      page.evaluate(function() {
+        document.getElementById('nav-link-yourAccount').click()
+      })
+      page.render('images/click.png')
+    }, 2000)
   },
   function() {
     console.log('sign in')
-    page.evaluate(function(email, password) {
-      document.getElementById('ap_email').value = email
-      document.getElementById('ap_password').value = password
-    }, email, password)
-
-    // setTimeout(function() {
-    //   page.evaluate(function() {
-    //     document.getElementById('signInSubmit').click()
-    //   })
-    // }, 1000)
-
-    page.render('signin.js')
-    console.log('done')
+    setTimeout(function() {
+      page.render('images/signin.png')
+      page.evaluate(function(email, password) {
+        document.getElementById('ap_email').value = email
+        document.getElementById('ap_password').value = password
+        document.getElementById('signInSubmit').click()
+      }, email, password)
+      page.render('images/signined.png')
+    }, 2000)
   },
   function() {
-    page.open('https://www.amazon.co.jp/Amazon-co-jp%E9%99%90%E5%AE%9A-%E3%83%9B%E3%82%B0%E3%83%AF%E3%83%BC%E3%83%84MAP%E4%BB%98%E3%81%8D-%E3%83%8F%E3%83%AA%E3%83%BC-%E3%83%9D%E3%83%83%E3%82%BF%E3%83%BC%E3%81%A8%E5%91%AA%E3%81%84%E3%81%AE%E5%AD%90-%E7%AC%AC%E4%B8%80%E9%83%A8-%E7%AC%AC%E4%BA%8C%E9%83%A8-%E7%89%B9%E5%88%A5%E3%83%AA%E3%83%8F%E3%83%BC%E3%82%B5%E3%83%AB%E7%89%88-%E3%83%8F%E3%83%AA%E3%83%BC-%E3%83%9D%E3%83%83%E3%82%BF%E3%83%BC%E3%82%B7%E3%83%AA%E3%83%BC%E3%82%BA/dp/4863893469/', function(status) {
-      page.render('item.png')
-    })
-    phantom.exit()
+    setTimeout(function() {
+      page.open(itemUrl, function(status) {
+        //document.getElementById('oneClickBuyButton').click()
+        page.render('images/item.png')
+      })
+      setTimeout(function() {
+        phantom.exit()
+      }, 3000)
+    }, 2000)
   }
 ]).next();
